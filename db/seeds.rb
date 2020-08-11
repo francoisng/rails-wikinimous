e37d70a85faeae04c09c86104ebb3b81c16df778
Francois Nguyen <fnguyen90@gmail.com>
2020-08-11 14:53:06 +0200

    exo 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Lorem.sentence(word_count: 4), content: Faker::Lorem.sentences(number: 1))
+end
